9459f1dece11bcaf1c17276719190fd0944fcf21
vmora <vmora@ucenfotec.ac.cr>
2025-04-02 18:10:27 -0600

    [New] Pruebas basadas en datos

---
+++ Pruebas basadas en datos/registroUsuario.py
@@ -0,0 +1,19 @@
+import re
+
+# Registrar un usuario si los datos son válidos.
+def registrar_usuario(nombre: str, email: str, telefono: str) -> str:
+    
+    # Validación de nombre no vacío
+    if not nombre.strip():
+        return "ERROR_NOMBRE"
+    
+    # Validación básica de email
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return "ERROR_EMAIL"
+    
+    # Validación de   teléfono (9 dígitos numéricos)
+    if not telefono.isdigit() or len(telefono) != 8:
+        return "ERROR_TELEFONO"
+    
+    return "ÉXITO"
+
